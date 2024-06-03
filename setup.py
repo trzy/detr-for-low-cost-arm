8bc4690144254159457401bea716a2a5b3db953f
Bart Trzynadlowski <bart.trzy@gmail.com>
2024-06-03 13:15:43 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='detr',
+    version='0.0.1-low-cost-robot-arm',
+    packages=find_packages(),
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
